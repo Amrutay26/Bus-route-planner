a502f820b3e01b26f8958b4ba8f04e5f82efff84
Amruta <amrutayadav269@gmail.com>
2026-06-07 15:00:08 +0530

    Db  connection

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const mongoose=require('mongoose');
+
+
+const connectdb=async()=>{
+    try{
+await mongoose.connect(process.env.MONGO_URL);
+console.log("Database connected");
+    }
+    catch(err){
+        console.log(err);
+    }
+};
+
+module.exports=connectdb;
